6ab962d559f475bb62b6c821fa4806b36403a8d8
Vil McGann <vil.mcgann@fotosearch.com>
2023-08-02 00:03:15 -0700

    cleaned up code

--- webapp/pizza/views.py
+++ webapp/pizza/views.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, render_template, url_for, request, redirect, jsonify
 
 from webapp import db
-from webapp.pizza.forms import ToppingForm
 from webapp.pizza.models import Topping, Pizza
 
 pizza = Blueprint('pizza', __name__, template_folder='templates')
@@ -17,10 +16,10 @@ def index():
         pizzas=pizzas,
     )
 
+
 @pizza.route('/add_topping', methods=['GET', 'POST'])
 def add_topping():
     topping = Topping(name=request.form.get('name').lower())
-    # topping_exists = db.session.query(Topping).filter_by(name=topping.name).first()
     if not is_exists(Topping, topping.name):
         db.session.add(topping)
         db.session.commit()
@@ -51,24 +50,22 @@ def delete_topping():
 @pizza.route('/add_pizza', methods=['GET', 'POST'])
 def add_pizza():
     name = request.form.get('name').lower()
+
+    # Check if there isn't a pizza already with this name
     if is_exists(Pizza, name):
         return redirect(url_for('pizza.index'))
+
     pizza = Pizza(name=name)
     toppings = request.form.getlist('topping')
     toppings.sort()
-    all_pizzas = Pizza.query.all()
-    for p in all_pizzas:
-        pizza_toppings = [str(topping.id) for topping in p.toppings]
-        pizza_toppings.sort()
-        if pizza_toppings == toppings:
-            return redirect(url_for('pizza.index'))
 
-    for topping_id in toppings:
-        topping = db.session.query(Topping).filter_by(id=topping_id).first()
-        pizza.toppings.append(topping)
+    # Loop through every pizza to check if there isn't a pizza with this combination already
+    pizzas = Pizza.query.all()
+    is_duplicate = check_for_duplicate_toppings(pizzas, toppings)
+    if is_duplicate:
+        return redirect(url_for('pizza.index'))
 
-    db.session.add(pizza)
-    db.session.commit()
+    save(pizza, toppings)
     return redirect(url_for('pizza.index'))
 
 
@@ -77,25 +74,21 @@ def edit_pizza():
     name = request.form.get('name').lower()
     pizza = Pizza.query.filter_by(id=request.form.get('pizzaId')).first()
     pizza_exists = db.session.query(Pizza).filter_by(name=name).first()
+
+    # Check if there is already pizza with the same other than itself
     if pizza_exists and pizza.id != pizza_exists.id:
         return redirect(url_for('pizza.index'))
 
     toppings = request.form.getlist('topping')
     toppings.sort()
-    all_pizzas = Pizza.query.filter(Pizza.id != pizza.id)
-    for p in all_pizzas:
-        pizza_toppings = [str(topping.id) for topping in p.toppings]
-        pizza_toppings.sort()
-        if pizza_toppings == toppings:
-            return redirect(url_for('pizza.index'))
+    pizzas = Pizza.query.filter(Pizza.id != pizza.id)
+    is_duplicate = check_for_duplicate_toppings(pizzas, toppings)
+    if is_duplicate:
+        return redirect(url_for('pizza.index'))
 
     pizza.toppings.clear()
-    for topping_id in toppings:
-        topping = db.session.query(Topping).filter_by(id=topping_id).first()
-        pizza.toppings.append(topping)
     pizza.name = name
-    db.session.add(pizza)
-    db.session.commit()
+    save(pizza, toppings)
     return redirect(url_for('pizza.index'))
 
 
@@ -111,6 +104,10 @@ def delete_pizza():
 
 @pizza.route('/pizza_info', methods=['POST'])
 def pizza_info():
+    """
+    Handles ajax call to get selected pizza's toppings
+    :return: Pizza topping information
+    """
     pizza = Pizza.query.filter_by(id=request.form.get('pizza_id')).first()
     return jsonify({
         'toppings': [topping.name for topping in pizza.toppings],
@@ -119,4 +116,41 @@ def pizza_info():
 
 
 def is_exists(model, name):
+    """
+    Checks to see if item already exists in database
+    :param model: Model class
+    :param name: Item name
+    :return: Model class object
+    """
     return db.session.query(model).filter_by(name=name).first()
+
+
+def save(pizza, toppings):
+    """
+    Link many-to-many relationship and saves to database
+    :param pizza: Pizza object
+    :param toppings: Toppings associated with Pizza object
+    """
+    #
+    for topping_id in toppings:
+        topping = db.session.query(Topping).filter_by(id=topping_id).first()
+        pizza.toppings.append(topping)
+
+    db.session.add(pizza)
+    db.session.commit()
+
+
+def check_for_duplicate_toppings(pizzas, toppings):
+    """
+    Loop through every pizza and check if there isn't already a pizza with the same toppings
+    :param pizzas: List of pizzas
+    :param toppings: List of toppings
+    :return: Boolean
+    """
+    #
+    for pizza in pizzas:
+        pizza_toppings = [str(topping.id) for topping in pizza.toppings]
+        pizza_toppings.sort()
+        if pizza_toppings == toppings:
+            return True
+    return False
